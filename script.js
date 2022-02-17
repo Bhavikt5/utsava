e77933dab043c6249842466eb4a0a1943301166e
Bhavikt5 <91686940+Bhavikt5@users.noreply.github.com>
2022-02-17 15:55:10 +0530

    utsava index files

---
+++ script.js
@@ -0,0 +1,37 @@
+let searchForm = document.querySelector('.header-1 .icons .search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+};
+
+
+
+// ------- count-down -------- 
+
+let countDate = new Date('february 25, 2022 00:00:00').getTime();
+
+function CountDown(){
+
+    let now = new Date().getTime();
+    gap = countDate - now;
+
+    let second = 1000;
+    let minute = second * 60;
+    let hour = minute * 60;
+    let day = hour * 24;
+
+    let d = Math.floor(gap / (day));
+    let h = Math.floor((gap % (day)) / (hour));
+    let m = Math.floor((gap % (hour)) / (minute));
+    let s = Math.floor((gap % (minute)) / (second));
+
+    document.getElementById('day').innerText = d;
+    document.getElementById('hour').innerText = h;
+    document.getElementById('minute').innerText = m;
+    document.getElementById('second').innerText = s;
+
+}
+
+setInterval(function(){
+    CountDown();
+},1000)
